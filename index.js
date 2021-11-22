3bd98f45c0cb75d26864c310b04288aab1a59691
saphirevert <jerome.cosandey@gmail.com>
2021-11-22 10:24:04 +0100

    Inline query examples

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const {Telegraf} = require('telegraf');
+const { Telegraf } = require('telegraf');
+const { Markup } = require("telegraf");
+
 require('dotenv').config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
@@ -6,6 +8,36 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) =>{
     ctx.reply('Welcome my lovely friend')
 });
+
+bot.command("inline", (ctx) => {
+  return ctx.reply("<b>Coke</b> or <i>Pepsi?</i>", {
+    parse_mode: "HTML",
+    ...Markup.inlineKeyboard([
+      Markup.button.callback("Coke", "coke"),
+      Markup.button.callback("Pepsi", "pepsi"),
+    ]),
+  });
+});
+
+bot.on("callback_query", async (ctx) => {
+  console.log(ctx.update.callback_query.id);
+  switch (ctx.update.callback_query.data) {
+    case "coke":
+      ctx.reply("Good choice !");
+      break;
+
+    case "pepsi":
+      bot.telegram.answerCbQuery(
+        ctx.update.callback_query.id,
+        "Please make the good choice, otherwise you would loose..."
+      );
+      break;
+
+    default:
+      break;
+  }
+});
+
 
 //lunch
 bot.launch()
