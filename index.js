9d1b6b5a7fea171255bb8ec5096f832e991ccd2d
D4rkHeart <leonardo.surdez@gmail.com>
2021-12-03 10:32:35 +0100

    [feat] Add current and next week display

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start((ctx) =>{
-    ctx.reply('Welcome my lovely friend')
+    ctx.reply('Welcome my lovely computer scientist')
 });
 
 //DATE
@@ -26,6 +26,18 @@ function nextMonday() {
   return date;
 }
 
+//GET WEEK AND NEXT WEEK
+function currentWeekNumber(increment){
+  let startDate = new Date();
+  let onejan = new Date(startDate.getFullYear(), 0, 1);
+  let week = Math.ceil((((startDate.getTime() - onejan.getTime()) / 86400000) + onejan.getDay() + 1) / 7 );
+  week += increment 
+  if(week > 52){
+    week = week -52;
+  }
+ return week
+}
+
 //INLINE QUERY //MAIN MENU
 bot.command("test", ctx =>{
     ctx.deleteMessage();
@@ -33,18 +45,18 @@ bot.command("test", ctx =>{
     bot.telegram.sendMessage(ctx.chat.id, startMainMenu, 
     {
         reply_markup:{
-            inline_keyboard: [
+            inline_keyboard: [  
                 [
-                  { text: "Semaine1", callback_data : "week1"}
+                  { text: `Semaine : ${currentWeekNumber(0)}`, callback_data : "week1"}
                 ],
                 [
-                  { text: "Semaine2", callback_data : "week2"}
+                  { text: `Semaine : ${currentWeekNumber(1)}`, callback_data : "week2"}
                 ],
                 [
-                  { text: "Semaine3", callback_data : "week3"}
+                  { text: `Semaine : ${currentWeekNumber(2)}`, callback_data : "week3"}
                 ],
                 [
-                  { text: "Semaine4", callback_data : "week4"}
+                  { text: `Semaine : ${currentWeekNumber(3)}`, callback_data : "week4"}
                 ],
                 [
                   { text: "<--", callback_data : "back"},
@@ -62,52 +74,55 @@ bot.command("test2", ctx =>{
   let startMainMenu = "Veuillez selectionner les jours souhaité";
   bot.telegram.sendMessage(ctx.chat.id, startMainMenu, 
   {
-      reply_markup:{
-          inline_keyboard: [
-              [
-                { text: "Date1", callback_data : "date1"}
-              ],
-              [
-                { text: "AM", callback_data : "date1_AM"},
-                { text: "PM", callback_data : "date1_PM"}
-              ],
-              [
-                { text: "Date2", callback_data : "date2"}
-              ],
-              [
-                { text: "AM", callback_data : "date2_AM"},
-                { text: "PM", callback_data : "date2_PM"}
-              ],
-              [
-                { text: "Date3", callback_data : "date3"}
-              ],
-              [
-                { text: "AM", callback_data : "date3_AM"},
-                { text: "PM", callback_data : "date3_PM"}
-              ],
-              [
-                { text: "Date4", callback_data : "date4"}
-              ],
-              [
-                { text: "AM", callback_data : "date4_AM"},
-                { text: "PM", callback_data : "date4_PM"}
-              ],
-              [
-                { text: "Date5", callback_data : "date5"}
-              ],
-              [
-                { text: "AM", callback_data : "date5_AM"},
-                { text: "PM", callback_data : "date5_PM"}
-              ],
-              [
-                { text: "<--", callback_data : "back"},
-              ]
-          ]
-      }
+    reply_markup:{
+        inline_keyboard: [
+            [
+              { text: "Lundi : ${date}", callback_data : "date1"}
+            ],
+            [
+              { text: "AM", callback_data : "date1_AM"},
+              { text: "PM", callback_data : "date1_PM"}
+            ],
+            [
+              { text: "Date2", callback_data : "date2"}
+            ],
+            [
+              { text: "AM", callback_data : "date2_AM"},
+              { text: "PM", callback_data : "date2_PM"}
+            ],
+            [
+              { text: "Date3", callback_data : "date3"}
+            ],
+            [
+              { text: "AM", callback_data : "date3_AM"},
+              { text: "PM", callback_data : "date3_PM"}
+            ],
+            [
+              { text: "Date4", callback_data : "date4"}
+            ],
+            [
+              { text: "AM", callback_data : "date4_AM"},
+              { text: "PM", callback_data : "date4_PM"}
+            ],
+            [
+              { text: "Date5", callback_data : "date5"}
+            ],
+            [
+              { text: "AM", callback_data : "date5_AM"},
+              { text: "PM", callback_data : "date5_PM"}
+            ],
+            [
+              { text: "<--", callback_data : "back"},
+            ]
+        ]
+    }
   }
   )
 })
 
+bot.action("week1", async (ctx) => {
+  console.log("toto")
+})
 
 // bot.on("callback_query", async (ctx) => {
 //   console.log(ctx.update.callback_query.id);
